5ca0c2c41e1008fdec0b9e5812c34801339a67a0
毛家兴 <908156949@qq.com>
2019-08-29 12:35:56 +0800

    first commit

---
+++ src/main/java/com/github/maojx0630/snowFlakeZk/SnowFlakeZkSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.github.maojx0630.snowFlakeZk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowFlakeZkSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnowFlakeZkSpringBootStarterApplication.class, args);
+	}
+
+}
